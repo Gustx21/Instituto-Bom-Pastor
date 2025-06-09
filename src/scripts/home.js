b6d61824eecd90d33c88da106630bfaa1e9d3c46
gustavo andré <gustavo.andrs21@gmail.com>
2025-06-09 19:47:07 -0300

    :construction: Add new funcionality and ajuste new fixes project

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,23 +1,3 @@
-// This script handles the hamburger menu toggle functionality
-// and closes the menu when clicking outside of it.
-
-const hamburger = document.getElementById('hamburger');
-const menu = document.querySelector('.menu');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    hamburger.classList.toggle('active');
-});
-
-window.addEventListener('click', (event) => {
-    if (!menu.contains(event.target) && !hamburger.contains(event.target)) {
-        menu.classList.remove('active');
-        hamburger.classList.remove('active');
-    }
-});
-
-// GOQR.ME/API
-
 // This script is used to show a carousel of images with next and previous buttons.
 
 const slides = document.querySelectorAll('.overlay');
@@ -60,7 +40,7 @@ const donateButtons = document.querySelectorAll('.donate-btn');
 
 donateButtons.forEach(button => {
     button.addEventListener('click', () => {
-        window.open('pages/donate.html', '_self');
+        window.open('pages/donate.html', '_parent');
     });
 });
 
@@ -69,18 +49,9 @@ donateButtons.forEach(button => {
 // It uses the 'pages/projects.html' path to redirect users.
 
 const learnMoreButtons = document.querySelectorAll('.learn-more-btn');
+
 learnMoreButtons.forEach(button => {
     button.addEventListener('click', () => {
-        window.open('pages/projects.html', '_self');
-    });
-});
-
-// This script handles the about button functionality
-// which redirects to the about page.
-
-const aboutButtons = document.querySelectorAll('.about-btn');
-aboutButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        window.open('pages/about.html', '_self');
+        window.open('pages/about.html', '_parent');
     });
 });
